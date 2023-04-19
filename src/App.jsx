cb2ca7101cf3eb482946e2e52d2ad733977964d7
Juan SB <juansb.dev@gmail.com>
2023-04-19 16:04:51 -0300

    deplo

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,12 @@ function App() {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
   const [darkMode, setDarkMode] = useState(false); // estado para manejar el modo oscuro
   const [cityUrl, setCityUrl] = useState('Cordoba'); // estado para manejar el ciudad
+  const [lugarDos, setLugarDos] = useState(''); // estado para manejar el ciudad
+
   let img_sol = 'https://cdn-icons-png.flaticon.com/512/6661/6661565.png'
   let lugar = cityUrl;
   let url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${lugar}`
-  
+  let lugarListo = ''
   useEffect(() => {
     const options = {
       method: 'GET',
@@ -42,35 +44,36 @@ function App() {
         clearInterval(intervalId)
       }
     }, [cityUrl]);
-        
-        // función para manejar el cambio de tema
-        const inputUrl = (event) => {
-          setCityUrl(event.target.value) 
+    //SET CIUDAD DESDE EL INPUT
 
-        }
-        const toggleTheme = () => {
-          setDarkMode(!darkMode);
-          document.body.classList.toggle('dark');
-        }
-        
+    const inputUrl = (event) => {
+      setCityUrl(event.target.value) 
+    }
+    // función para manejar el cambio de tema
+    const toggleTheme = () => {
+      setDarkMode(!darkMode);
+      document.body.classList.toggle('dark');
+    }
+    //set placeholder
+    const inP = ( )=>{
+      setLugarDos(this.target.value)
+
+    }
+    
   return (
     <Container>
     <Row> 
-    <input id='lugar-inp' type="text" onKeyDown={event => {
-  if (event.key === "Enter") {
-    inputUrl(event);
-  }
-}} placeholder={lugar } />
+    
     </Row>
   {myResponse && lugar && (
     <Container>
     <Row>
        <Buenas > </Buenas>
-        <h2>{currentTime}</h2>
     </Row>
     <Row>
       <Col>
         <Dias text={ myResponse.current.is_day}></Dias>
+        <h2>{currentTime}</h2>
       </Col>
     </Row>
     <Row>
@@ -83,7 +86,16 @@ function App() {
         <img src={myResponse.current.condition.icon} onClick={toggleTheme} alt="icono" />
       </Col>
     </Row>
-    <h3> {myResponse.location.name},  {myResponse.location.region} </h3>
+    <h1>
+       <input id='lugar-inp' type="text" onChange={inP} placeholder={lugarDos} onKeyDown={event => {
+
+         if (event.key === "Enter") {
+           inputUrl(event);
+
+          }
+        }}  />
+        </h1>
+    <h3>   {myResponse.location.region} </h3>
 
     <Button className='asd' variant='secondary' onClick={toggleTheme}>
       <img src={img_sol} alt="" />
